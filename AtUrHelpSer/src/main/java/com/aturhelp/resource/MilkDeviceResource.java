5c437c31475dbb4f6bf07769a2608f061c5e2870
Ramalingeshwar.vutukuru <KH960@kitspl.com>
2016-03-22 21:37:21 +0530

    Added route support for device

--- AtUrHelpSer/src/main/java/com/aturhelp/resource/MilkDeviceResource.java
+++ AtUrHelpSer/src/main/java/com/aturhelp/resource/MilkDeviceResource.java
@@ -1,8 +1,15 @@
 package com.aturhelp.resource;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -11,6 +18,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.aturhelp.common.Login;
+import com.aturhelp.common.milk.Route;
 import com.aturhelp.services.MilkService;
 
 @Component
@@ -26,14 +34,22 @@ final static Logger LOG = Logger.getLogger(MilkDeviceResource.class);
 		@POST
 		@Path("lv")
 		@Consumes({ MediaType.APPLICATION_JSON })
-		public Response loginValidation(Login login) {
-			System.out.println("Inside the login method");
+		public Response loginValidation(Login login, @Context HttpServletRequest request) {
+			HttpSession session = request.getSession();
+			session.setAttribute("user", login.getUserName());
 			boolean res = milkService.validateLogin(login);
 			if (res) {
 				return Response.ok(0 + "").build();
 			} else {
 				return Response.ok(1 + "").build();
 			}
+		}
+		
+		@GET
+		@Path("routedetails")
+		@Produces({ MediaType.APPLICATION_JSON })
+		public List<Route> getRouteDetails() {
+			return milkService.getRoutes();
 		}
 
 }
